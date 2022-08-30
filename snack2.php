cc634884886f3f318019a3aa1ccc3f9bc049a103
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-08-30 16:51:40 +0200

    snack 2 completed

---
+++ snack2.php
@@ -0,0 +1,26 @@
+<?php 
+$name = $_GET['name'] ?? '';
+$mail = $_GET['mail'] ?? '';
+$age = $_GET['age'];
+
+$has_success = false;
+
+if (strlen(trim($name)) > 3 && (str_contains($mail, '@') && str_contains($mail, '.')) && is_numeric($age)) $has_success = true;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if($has_success) : ?>
+        <h2>Accesso Riuscito</h2>
+    <? else : ?>
+        <h2>Accesso Negato</h2>
+    <? endif; ?>
+</body>
+</html>
